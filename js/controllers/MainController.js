7112bb82f12f21e501069fda57514aa3213bca95
Matt Calvert <xander.raymatt@gmail.com>
2020-12-01 09:29:37 +0000

    ISBN commit

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,18 +1,39 @@
 app.controller("MainController", [
 	"$scope",
-	function ($scope) {
-		$scope.list = ["Learn Angular", "Learn C#", "Learn SQL"];
+	"$http",
+	function ($scope, $http) {
+		$scope.todo = {
+			title: "Things I need to do",
+			list: ["Learn Angular", "Learn C#", "Learn SQL"],
+		};
+		$scope.books = {
+			title: "Books I need to read",
+			list: [],
+		};
 
-		$scope.addItem = function () {
-			$scope.list.push($scope.addToDo);
+		// Regular Expression (only inputs with digits will match) will return bool 0/1
+		function hasOnlyNumbers(item) {
+			return /^[0-9]*$/.test(item);
+		}
+
+		$scope.addItem = function (itemList, item) {
+			//ISBN : 10/13 in length and only numbers
+			if ((item.length == 10 || item.length == 13) && hasOnlyNumbers(item)) {
+				console.log("ISBN");
+				$http
+					.get("https://www.googleapis.com/books/v1/volumes?q=isbn:" + item)
+					.success(function (data) {
+						itemList.push(
+							"Title: " +
+								data.items[0].volumeInfo.title +
+								" // Author(s):" +
+								data.items[0].volumeInfo.authors
+						);
+					});
+			} else {
+				console.log("Not an ISBN");
+				itemList.push(item);
+			}
 		};
 	},
 ]);
-
-// app.controller('MainController', ['$scope', function($scope) {
-//     $scope.list = ["Clean my room", "Go to the store", "Study Cracking the Coding Interview"]
-
-//     $scope.addItem = function() {
-//         $scope.list.push($scope.addToDo);
-//     }
-// }]);
